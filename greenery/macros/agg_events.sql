a4e32a4bf0da14883e623320d27ee9bd8f5b138d
cecilia depman <cecilia.depman@murmuration.org>
2024-11-03 17:46:34 +0000

    macro rename

--- greenery/macros/agg_events.sql
+++ greenery/macros/agg_events.sql
@@ -1,6 +1,4 @@
 {# This function applies an aggregate operation to an event_type #}
 {% macro agg_events(event_type, agg_op) %} 
-
-{{agg_op}}(case when event_type='{{event_type}}' then 1 else 0 end) 
-
-{% endmacro %} 
+  {{ agg_op }}(CASE WHEN event_type = '{{ event_type }}' THEN 1 ELSE 0 END)
+{% endmacro %}
